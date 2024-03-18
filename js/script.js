9c05ef9396df75e7a3971edce5e079dd390acd72
Joacim Andersson <joacim@josiasmusik.se>
2024-03-18 10:16:08 +0100

    made slider slide and more responsiveness

--- js/script.js
+++ js/script.js
@@ -2,26 +2,24 @@ const chevronDown = document.querySelectorAll('.fa-chevron-down');
 const rows = document.querySelectorAll('.accordion__row');
 const slides = document.querySelectorAll('.slides img');
 const slideSelectors = document.querySelectorAll('.slide-nav__circle');
+const slide = document.querySelector('.slide');
+const slidesContainer = document.querySelector('.slides');
 
 const showSlide = (id) => {
-  slides[id].classList.add('display-slide')
-  slideSelectors[id].classList.add('active-slide')
+  slides[id].classList.add('display-slide');
+  slideSelectors[id].classList.add('active-slide');
 };
 
-const prevSlide = () => {};
-
-const nextSlide = () => { };
-
 const changeSlide = (e) => {
-  slideSelectors.forEach(selector => {
-    selector.classList.remove('active-slide')
-  })
-  slides.forEach(slide => {
-    slide.classList.remove('display-slide')
-  })
+  slideSelectors.forEach((selector) => {
+    selector.classList.remove('active-slide');
+  });
+  slides.forEach((slide) => {
+    slide.classList.remove('display-slide');
+  });
 
-  showSlide(e.target.id)
-}
+  showSlide(e.target.id);
+};
 
 const toggleAccordionRow = (e) => {
   const row = e.target.parentElement.parentElement;
@@ -49,16 +47,38 @@ const toggleAccordionRow = (e) => {
   }
 };
 
+const changeSlideNew = (e) => {
+  const slideWidth = slide.clientWidth;
+  const slideId = e.target.id;
+
+  slideSelectors.forEach((selector) => {
+    selector.classList.remove('active-slide');
+  });
+
+  slideSelectors[slideId].classList.add('active-slide');
+
+  switch (+slideId) {
+    case 0:
+      slidesContainer.scrollLeft = 0;
+      break;
+    case 1:
+      slidesContainer.scrollLeft = slideWidth;
+      break;
+    case 2:
+      slidesContainer.scrollLeft += slideWidth * 2;
+  }
+};
+
 const init = () => {
   chevronDown.forEach((chev) => {
     chev.addEventListener('click', toggleAccordionRow);
   });
 
-  slideSelectors.forEach(selector => selector.addEventListener('click', (e) => changeSlide(e)))
+  slideSelectors.forEach((selector) =>
+    selector.addEventListener('click', (e) => changeSlideNew(e))
+  );
 
-  slides[0].classList.add('display-slide');
-  slideSelectors[0].classList.add('active-slide')
+  slideSelectors[0].classList.add('active-slide');
 };
 
 document.addEventListener('DOMContentLoaded', init);
-
